3e4187cd58b834f6770f64edb2bcecd772939cdd
Андрій <gapeev1979@gmail.com>
2025-11-14 00:20:47 +0200

    added routs on cards and desk/tab/mob 4:6

--- app/(public routes)/categories/page.tsx
+++ app/(public routes)/categories/page.tsx
@@ -5,28 +5,56 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import CategoriesPage from "./page-client";
-import { headers } from "next/headers";
 
 export default async function Page() {
   const queryClient = new QueryClient();
-
   const initialPage = 1;
-
-  const headersList = await headers();
-  const userAgent = headersList.get("user-agent") || "";
-
-  const isMobileOrTablet = /Mobi|Android|iPhone|iPad/i.test(userAgent);
-
-  const limit = isMobileOrTablet ? 4 : 6;
-
+  // const limit = 6;
   await queryClient.prefetchQuery({
-    queryKey: ["categories", initialPage, limit],
-    queryFn: () => getCategories(initialPage, limit),
+    queryKey: ["categories", initialPage],
+    queryFn: () => getCategories(initialPage),
   });
-
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <CategoriesPage initialPage={initialPage} limit={limit} />
+      <CategoriesPage initialPage={initialPage} />
     </HydrationBoundary>
   );
 }
+
+////////////////////////////////////////////////////////////////
+// зчитує параметри екрана - не корректно - переробити
+////////////////////////////////////////////////////////////////
+// import { getCategories } from "@/lib/api/api";
+// import {
+//   dehydrate,
+//   HydrationBoundary,
+//   QueryClient,
+// } from "@tanstack/react-query";
+// import CategoriesPage from "./page-client";
+// import { headers } from "next/headers";
+
+// export default async function Page() {
+//   const queryClient = new QueryClient();
+
+//   const initialPage = 1;
+
+//   const headersList = await headers();
+//   const userAgent = headersList.get("user-agent") || "";
+
+//   const isMobileOrTablet = /Mobi|Android|iPhone|iPad/i.test(userAgent);
+
+//   const limit = isMobileOrTablet ? 4 : 6;
+
+//   await queryClient.prefetchQuery({
+//     queryKey: ["categories", initialPage, limit],
+//     queryFn: () => getCategories(initialPage, limit),
+//   });
+
+//   return (
+//     <HydrationBoundary state={dehydrate(queryClient)}>
+//       <CategoriesPage initialPage={initialPage} limit={limit} />
+//     </HydrationBoundary>
+//   );
+// }
+//////////////////////////////////////////////////
+/////////////////////////////////////////////////
