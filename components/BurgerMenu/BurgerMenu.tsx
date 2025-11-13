3a1987736a4b6ed4acb66b64666622ddd594708c
Tetiana <Korostijtatiana@gmail.com>
2025-11-13 19:54:17 +0200

    add burger login

--- components/BurgerMenu/BurgerMenu.tsx
+++ components/BurgerMenu/BurgerMenu.tsx
@@ -1,51 +1,57 @@
-import Link from "next/link"
-import css from "./BurgerMenu.module.css"
-import { useAuthStore } from "@/stores/authStore"
+import Link from "next/link";
+import css from "./BurgerMenu.module.css";
+import { useAuthStore } from "@/stores/authStore";
 
 interface BurgerMenuProps {
-	menuOpen: boolean
-	onClose: () => void
+  menuOpen: boolean;
+  onClose: () => void;
 }
 
 export default function BurgerMenu({ menuOpen, onClose }: BurgerMenuProps) {
-	const user = useAuthStore((state) => state.user)
-	const isAuthenticated = useAuthStore((state) => state.isAuthenticated)
+  const user = useAuthStore((state) => state.user);
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 
-	return (
-		<div className={`${css.burgerMenu} ${menuOpen ? css.active : ""}`}>
-			<ul className={css.burgerNav}>
-				<li>
-					<Link href="/" onClick={onClose}>
-						Головна
-					</Link>
-				</li>
-				<li>
-					<Link href="/goods" onClick={onClose}>
-						Товари
-					</Link>
-				</li>
-				<li>
-					<Link href="/categories" onClick={onClose}>
-						Категорії
-					</Link>
-				</li>
-			</ul>
-			<div className={css.BurgerAuth}>
-				{!isAuthenticated ? (
-					<>
-						<Link href="/sign-in" onClick={onClose} className={css.BurgerNavUp}>
-							Вхід
-						</Link>
-						<Link href="/sign-up" onClick={onClose} className={css.BurgerNavIn}>
-							Реєстрація
-						</Link>{" "}
-					</>
-				) : (
-					<Link href="/profile" onClick={onClose} className={css.BurgerNavUpBasket}>
-						Кабінет
-					</Link>
-				)}
-			</div>{" "}
-		</div>
-	)
+  return (
+    <div className={`${css.burgerMenu} ${menuOpen ? css.active : ""}`}>
+      <ul className={css.burgerNav}>
+        <li>
+          <Link href="/" onClick={onClose}>
+            Головна
+          </Link>
+        </li>
+        <li>
+          <Link href="/goods" onClick={onClose}>
+            Товари
+          </Link>
+        </li>
+        <li>
+          <Link href="/categories" onClick={onClose}>
+            Категорії
+          </Link>
+        </li>
+      </ul>
+
+      <div className={css.BurgerAuth}>
+        {/* 👇 Та сама логіка, що й у Header */}
+        {!isAuthenticated ? (
+          <>
+            <Link href="/sign-in" onClick={onClose} className={css.BurgerNavUp}>
+              Вхід
+            </Link>
+            <Link href="/sign-up" onClick={onClose} className={css.BurgerNavIn}>
+              Реєстрація
+            </Link>
+          </>
+        ) : (
+          <Link
+            href="/profile"
+            onClick={onClose}
+            className={css.BurgerNavUpBasket}
+          >
+            Кабінет
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 }
