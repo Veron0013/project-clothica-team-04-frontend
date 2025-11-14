20cc1ea9d12d68db89a6c0817c80d963d84c6e07
Mr.Igor <iv_mirsoft@ukr.net>
2025-11-14 23:41:13 +0200

    order fix++

--- app/(public routes)/order/page.tsx
+++ app/(public routes)/order/page.tsx
@@ -1,20 +1,3 @@
-//"use client"
-
-//import { useAuthStore } from "@/stores/authStore"
-//import { useBasket } from "@/stores/basketStore"
-//import { useRouter } from "next/navigation"
-
-const OrderPage = () => {
-	//const user = useAuthStore((state) => state.user)
-	//const { goods, addGood, decrementGood, clearBasket, removeGood, updateGoodQuantity } = useBasket()
-
-	//const router = useRouter()
-
-	//const handleSubmit = (FormData: FormData) => {
-	//	const text = FormData.get("text") as string
-	//	console.log(text)
-	//}
+export default async function CategoryPage() {
 	return null
 }
-
-export default OrderPage()
