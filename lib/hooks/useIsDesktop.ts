b0942b98b800a68e5f5f7369694ba7305d247b39
Андрій <gapeev1979@gmail.com>
2025-11-14 22:52:27 +0200

    fix metedata and clean comments

--- lib/hooks/useIsDesktop.ts
+++ lib/hooks/useIsDesktop.ts
@@ -1,26 +1,21 @@
 import { useState, useEffect } from "react";
 
-// Точка перелому, від якої починаємо завантажувати 6 карток
 const DESKTOP_BREAKPOINT = 1440;
-// Базовий ліміт для менших екранів
 const BASE_LIMIT = 4;
-// Ліміт для великих екранів
 const DESKTOP_LIMIT = 6;
 
 export const useIsDesktop = () => {
-  // Використовуємо 0 або undefined як початкове значення для запобігання SSR-помилкам
   const [limit, setLimit] = useState(0);
 
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth >= DESKTOP_BREAKPOINT) {
-        setLimit(DESKTOP_LIMIT); // >= 1440px -> 6 карток
+        setLimit(DESKTOP_LIMIT);
       } else {
-        setLimit(BASE_LIMIT); // < 1440px -> 4 картки
+        setLimit(BASE_LIMIT);
       }
     };
 
-    // Встановлюємо початкове значення
     handleResize();
 
     window.addEventListener("resize", handleResize);
