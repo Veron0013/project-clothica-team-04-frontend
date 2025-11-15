d22ca872e4ddd030caf170217a97e52ccb9ab264
Андрій <gapeev1979@gmail.com>
2025-11-15 19:55:48 +0200

    page not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,34 +1,42 @@
-import { Metadata } from "next"
-import Link from "next/link"
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import css from "./NotFound.module.css";
 
 export const metadata: Metadata = {
-	title: "404 — Page not found | Notehub app",
-	description: "Page not found. Sorry",
-	openGraph: {
-		title: `404 - Notehub app`,
-		description: "Page not found.Sorry",
-		url: `https://notehub-app-auth.vercel.app`,
-		siteName: "NoteHub",
-		images: [
-			{
-				url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-				width: 1200,
-				height: 630,
-				alt: "404 - Notehub",
-			},
-		],
-		type: "website",
-	},
-}
+  title: `404 - Сторінку на Clothica не знайдено`,
+  description: "Такої сторінки не існує.",
+  openGraph: {
+    title: `404 - Сторінку на Clothica не знайдено`,
+    description: "Такої сторінки не існує.",
+    url: `https://clothica-team-04-frontend.vercel.app/`,
+    siteName: "Clothica",
+    type: "website",
+  },
+};
 
 const NotFound = () => {
-	return (
-		<div>
-			<Link href="/">Go back home</Link>
-			<h1>404 - Page not found</h1>
-			<p>Sorry, the page you are looking for does not exist.</p>
-		</div>
-	)
-}
+  return (
+    <div className={css.NotFoundContainer}>
+      <h1 className={css.NotFoundTitle}>ОПАНЬКИ!!!</h1>
+      <p className={css.NotFoundTitle}>404</p>
 
-export default NotFound
+      <Image
+        src="https://res.cloudinary.com/dyounr2tf/image/upload/v1763228561/%D0%97%D0%BD%D1%96%D0%BC%D0%BE%D0%BA_%D0%B5%D0%BA%D1%80%D0%B0%D0%BD%D0%B0_2025-11-15_194135_rcpesh.png"
+        alt="img"
+        width={250}
+        height={250}
+        className={css.NotFoundImage}
+      />
+      <h3 className={css.NotFoundDescr}>Сторінку не знайдено!!!</h3>
+      <p className={css.NotFoundText}>Ви перейшли на неїснуюче посилання.</p>
+      <p className={css.NotFoundText}>Спробуйте з головної сторінки.</p>
+
+      <Link href="/" className={css.NotFoundLink}>
+        На головну сторінку
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
