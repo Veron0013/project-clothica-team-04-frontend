79dcc2c87906610945c51f88084d327ed61a7d98
Mr.Igor <iv_mirsoft@ukr.net>
2025-11-17 18:44:19 +0200

    style fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,86 +1,84 @@
-import type { Metadata } from "next";
-import { Inter, Nunito_Sans } from "next/font/google";
-import "./globals.css";
-import { Suspense } from "react";
-import Loading from "./loading";
-import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
-import AuthProvider from "@/components/AuthProvider/AuthProvider";
-import AuthPagesLayout from "@/components/AuthPagesLayout/AuthPagesLayout";
-import { ThemeProvider } from "@/components/ThemeProvider/ThemeProvider";
+import type { Metadata } from "next"
+import { Inter, Nunito_Sans } from "next/font/google"
+import "./globals.css"
+import { Suspense } from "react"
+import Loading from "./loading"
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+import { Toaster } from "react-hot-toast"
+import AuthProvider from "@/components/AuthProvider/AuthProvider"
+import AuthPagesLayout from "@/components/AuthPagesLayout/AuthPagesLayout"
+import { ThemeProvider } from "@/components/ThemeProvider/ThemeProvider"
 
 const interSans = Inter({
-  variable: "--font-inter",
-  subsets: ["latin", "cyrillic"],
-  display: "swap",
-  preload: true,
-});
+	variable: "--font-inter",
+	subsets: ["latin", "cyrillic"],
+	display: "swap",
+	preload: true,
+})
 
 const nunitoSans = Nunito_Sans({
-  variable: "--font-nunito-mono",
-  subsets: ["latin", "cyrillic"],
-  display: "swap",
-  preload: true,
-});
+	variable: "--font-nunito-mono",
+	subsets: ["latin", "cyrillic"],
+	display: "swap",
+	preload: true,
+})
 
 export const metadata: Metadata = {
-  title: "Clothica - best clothers",
-  description: "Find your own style today",
-  openGraph: {
-    title: `Clothica - best clothers`,
-    description: "Find your own style today",
-    url: `https://movieDB.com`,
-    siteName: "Clothica",
-    images: [
-      {
-        url: "https://ac.goit.global/fullstack/react/movie DB-og-meta.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Clothica",
-      },
-    ],
-    type: "website",
-  },
-  icons: {
-    icon: "/favicon.svg",
-  },
-};
+	title: "Clothica - best clothers",
+	description: "Find your own style today",
+	openGraph: {
+		title: `Clothica - best clothers`,
+		description: "Find your own style today",
+		url: `https://movieDB.com`,
+		siteName: "Clothica",
+		images: [
+			{
+				url: "https://ac.goit.global/fullstack/react/movie DB-og-meta.jpg",
+				width: 1200,
+				height: 630,
+				alt: "Clothica",
+			},
+		],
+		type: "website",
+	},
+	icons: {
+		icon: "/favicon.svg",
+	},
+}
 
 export default function RootLayout({
-  children,
-  modal,
+	children,
+	modal,
 }: Readonly<{
-  children: React.ReactNode;
-  modal: React.ReactNode;
+	children: React.ReactNode
+	modal: React.ReactNode
 }>) {
-  return (
-    <html
-      lang="en"
-      data-scroll-behavior="smooth"
-      data-theme="light" // стартова тема
-      suppressHydrationWarning // щоб не було мутної помилки по темі
-    >
-      <body
-        className={`${interSans.variable} ${nunitoSans.variable} antialiased`}
-      >
-        <ThemeProvider>
-          <TanStackProvider>
-            <div className="layout">
-              <AuthProvider>
-                <Suspense fallback={<Loading />}>
-                  <AuthPagesLayout>
-                    {children}
-                    {modal}
-                  </AuthPagesLayout>
-                </Suspense>
-              </AuthProvider>
-            </div>
-            <Toaster />
-            <ReactQueryDevtools initialIsOpen={false} />
-          </TanStackProvider>
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			data-scroll-behavior="smooth"
+			data-theme="light" // стартова тема
+			suppressHydrationWarning // щоб не було мутної помилки по темі
+		>
+			<body className={`${interSans.variable} ${nunitoSans.variable} antialiased`}>
+				<ThemeProvider>
+					<TanStackProvider>
+						<div className="layout">
+							<AuthProvider>
+								<Suspense fallback={<Loading />}>
+									<AuthPagesLayout>
+										{children}
+										{modal}
+									</AuthPagesLayout>
+								</Suspense>
+							</AuthProvider>
+						</div>
+						<Toaster />
+						<ReactQueryDevtools initialIsOpen={false} />
+					</TanStackProvider>
+				</ThemeProvider>
+			</body>
+		</html>
+	)
 }
