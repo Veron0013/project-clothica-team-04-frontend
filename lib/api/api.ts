20362c7ce7b6a765d3f9be397e35a2a41e78fe05
Mr.Igor <iv_mirsoft@ukr.net>
2025-11-18 13:16:49 +0200

    profile fix

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,49 +1,61 @@
-import { CategoriesResponse } from "@/types/categories"
-import { AllFilters } from "@/types/filters"
-import { BasketStoreOrder, Good, GoodsQuery, GoodsResponse } from "@/types/goods"
-import { Order } from "@/types/orders"
-import axios, { AxiosError } from "axios"
+import { CategoriesResponse } from '@/types/categories';
+import { AllFilters } from '@/types/filters';
+import {
+  BasketStoreOrder,
+  Good,
+  GoodsQuery,
+  GoodsResponse,
+} from '@/types/goods';
+import { Order } from '@/types/orders';
+import axios, { AxiosError } from 'axios';
 
-export type ApiError = AxiosError<{ error: string }>
+export type ApiError = AxiosError<{ error: string }>;
 
 export const nextAuthServer = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_PROXY_API_URL + "/api",
-	withCredentials: true,
-})
+  baseURL: process.env.NEXT_PUBLIC_PROXY_API_URL + '/api',
+  withCredentials: true,
+});
 
 export const nextServer = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_API_URL,
-	withCredentials: true,
-})
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
+});
 
-export const getGoods = async (searchParams: GoodsQuery): Promise<GoodsResponse> => {
-	const response = await nextServer.get("/goods", { params: searchParams })
-	return response.data
-}
+export const getGoods = async (
+  searchParams: GoodsQuery
+): Promise<GoodsResponse> => {
+  const response = await nextServer.get('/goods', { params: searchParams });
+  return response.data;
+};
 
 export type BasketGoodsParams = {
-	goodIds: Good["_id"][]
-}
+  goodIds: Good['_id'][];
+};
 
-export const getGoodsFromArray = async (query: BasketGoodsParams): Promise<BasketStoreOrder[]> => {
-	const response = await nextServer.post("/goods/from-array", query)
-	return response.data
-}
+export const getGoodsFromArray = async (
+  query: BasketGoodsParams
+): Promise<BasketStoreOrder[]> => {
+  const response = await nextServer.post('/goods/from-array', query);
+  return response.data;
+};
 
-export const getCategories = async (page: number, limit?: number): Promise<CategoriesResponse> => {
-	const response = await nextServer.get("/categories", {
-		params: { page, limit },
-	})
+export const getCategories = async (
+  page: number,
+  limit?: number
+): Promise<CategoriesResponse> => {
+  const response = await nextServer.get('/categories', {
+    params: { page, limit },
+  });
 
-	return response.data
-}
+  return response.data;
+};
 
 export const getFilterOptions = async (): Promise<AllFilters> => {
-	const response = await nextServer.get("/goods/all-filters")
-	return response.data
-}
+  const response = await nextServer.get('/goods/all-filters');
+  return response.data;
+};
 
 export const getUserOrders = async (): Promise<Order[]> => {
-	const response = await nextAuthServer.get<Order[]>(`/orders`)
-	return response.data
-}
+  const response = await nextAuthServer.get<Order[]>(`/order`);
+  return response.data;
+};
